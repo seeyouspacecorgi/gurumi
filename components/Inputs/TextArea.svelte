02888203baf176e7ec033ae744ad5418adb0bbac
dacey777 <42503711+dacey777@users.noreply.github.com>
2021-10-15 15:37:06 +0200

    Fix textarea css

--- components/Inputs/TextArea.svelte
+++ components/Inputs/TextArea.svelte
@@ -11,43 +11,45 @@
 
 <article>
   <output bind:this={output}>{@html styled}<br/></output>
-  <textarea bind:value={raw} spellcheck="false" wrap="soft" on:scroll={update_scroll} on:change={update_scroll} on:blur></textarea>
   <textarea bind:value={raw} spellcheck="false" on:scroll={update_scroll} on:change={update_scroll} on:blur></textarea>
 </article>
 
 <style>
   /* TODO: prevent horizontal scrolling */
   article {
-    border: solid 2px;
-    position: relative;
     flex:1;
-    font-family: var(--font-highlight);
+    position: relative;
+    border: solid 2px;
+    border-radius: var(--ui-radius);
   }
-  textarea, output {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-    border-radius: 0;
-    outline: none;
-    border: none;
+  textarea,
+  output {
+    padding: 1em;
     overflow-y: auto;
-    overflow-x: hidden;
-    width: 100%;
-    height: 100%;
-    vertical-align: bottom;
-    white-space: pre;
-    word-wrap: break-word;
-  }
-  textarea {
-    resize: none;
+    /* reset */
     line-height: inherit;
     font-size: inherit;
     font-family: inherit;
+    white-space: break-spaces;
+    box-sizing: border-box;
+    margin: 0;
+    border-radius: 0;
+    outline: none;
+    border: none;
+  }
+  textarea {
+    resize: none;
+    height: 100%;
+    width: 100%;
     background: transparent;
   }
   output {
     position: absolute;
     z-index: -1;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
     color: transparent;
   }
 </style>
